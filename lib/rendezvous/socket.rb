7790d2253a2331bb784b2ad45cd5fde4fd14b9bc
Michael Hale <mike@hales.ws>
2013-12-15 00:48:49 -0500

    log elapsed times

--- lib/rendezvous/socket.rb
+++ lib/rendezvous/socket.rb
@@ -16,6 +16,15 @@ module Rendezvous
         super()
       end
 
+      def log(args={})
+        start = Time.now
+        puts args.merge(at: :start)
+        result = yield
+        elapsed = Time.now - start
+        puts args.merge(at: :finish, elapsed: elapsed)
+        result
+      end
+
       # lport=0 causes OS to select a random high port
       def get_peer_endpoint(lport=0)
         socket = CustomSocket.new
@@ -26,15 +35,20 @@ module Rendezvous
         [socket.local_port, rhost, rport.to_i]
       end
 
-      def peer_socket
-        lport, rhost, rport = get_peer_endpoint
+      def peer_socket(lport, rhost, rport)
         peer = CustomSocket.new
         peer.bind(lport)
         peer if peer.connect(rhost, rport)
       end
 
       def open
-        peer_socket
+        lport, rhost, rport = nil
+        log(step: :get_peer_endpoint) {
+          lport, rhost, rport = get_peer_endpoint
+        }
+        log(step: :connect) {
+          peer_socket(lport, rhost, rport)
+        }
       end
     end
   end
